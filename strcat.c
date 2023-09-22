fa2d59bac407934561aeebaf2bce14baaeee44a2
JanefrancessC <janefrancesschioma@gmail.com>
2023-09-22 20:43:31 +0100

    completed strcat

--- strcat.c
+++ strcat.c
@@ -15,4 +15,19 @@ char *strcat(const char *dest, const char *src)
 	
 	if (dest == NULL || src == NULL)
 		return (NULL);
+
+	dest_len = _strlen(dest);
+	src_len = _strlen(src);
+
+	new_dest = malloc((dest_len + src_len +1) * sizeof(char));
+
+	if (new_dest == NULL) 
+		return (NULL);
+
+	_strcpy(new_dest, dest);
+	_strcpy(new_dest +dest_len, src);
+
+	free(dest);
+	
+	return (new_dest);
 }
