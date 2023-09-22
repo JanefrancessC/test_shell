1290b22eec8635626d250fa8040fab37bd41fecb
JanefrancessC <janefrancesschioma@gmail.com>
2023-09-22 20:01:18 +0100

    completing strdup

--- strdup.c
+++ strdup.c
@@ -7,4 +7,18 @@
 */
 
 char *_strdup(const char *str)
-{}
+{
+	char *new_str;
+	size_t len;
+
+	if (str == NULL)
+		return (NULL);
+	len = _strlen(str) + 1;
+	new_str = malloc(len * sizeof(char));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	_strcpy(new_str, str);
+	return new_str;
+}
